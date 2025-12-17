91e2cb04c3dfe9851e1fa92378b815b9ea925dc0
JM Mac <johnmarkmancol@gmail.com>
2025-12-18 03:07:29 +0800

    chore: logout

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,19 +1,43 @@
 <template>
   <UHeader title="Booking Conflict Checker">
     <template #right>
-      <UUser
-        :name="auth.user?.name"
-        :description="auth.user?.email"
-        :avatar="{
-          icon: 'i-lucide-user',
-        }"
-      />
+      <div class="flex gap-4 items-center">
+        <UUser
+          :name="auth.user?.name"
+          :description="auth.user?.email"
+          :avatar="{
+            icon: 'i-lucide-user',
+          }"
+        />
+        <UButton variant="ghost" @click="handleLogout()">Logout</UButton>
+      </div>
     </template>
   </UHeader>
 </template>
 
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth.store'
+import { useOverlay } from '@nuxt/ui/runtime/composables/useOverlay.js'
+import ConfirmationDialog from './ConfirmationDialog.vue'
+import { useRouter } from 'vue-router'
 
 const auth = useAuthStore()
+const router = useRouter()
+
+const overlay = useOverlay()
+const logoutModal = overlay.create(ConfirmationDialog)
+const handleLogout = () => {
+  logoutModal.open({
+    title: 'Logout',
+    message: 'Are you sure you want to logout?',
+    onOk: () => {
+      logout()
+    },
+  })
+}
+
+const logout = () => {
+  auth.logout()
+  router.push({ name: 'login' })
+}
 </script>
